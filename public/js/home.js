0772e2383ce9d615d1b937e3490547213fbb7caa
Ilya <shiroku12@mail.ru>
2022-10-05 10:44:16 +0300

    Rele 90%

--- public/js/home.js
+++ public/js/home.js
@@ -1,37 +1,33 @@
 const container = document.querySelector('.container');
+console.log('Privet');
 
 const req = async () => { // ! Прикепел, оказывается гугл блочит запрос
+  console.log('Poka');
   try {
-    const url = 'https://newsapi.org/v2/top-headlines?country=ru&apiKey=69f4633ca37b401eab74f2217a474d14';
+    const apiKey = '69f4633ca37b401eab74f2217a474d14';
+    const url = `
+    https://newsapi.org/v2/top-headlines?country=ru&apiKey=${apiKey}`;
     const response = await fetch(url);
-    console.log(response);
+    console.log('Error ==> ', response);
     const result = await response.json();
     console.log(result);
-
+    result.articles.length = 4;
     result.articles.forEach((article) => {
-        const newsDiv = document.createElement('div');
-        newsDiv.setAttribute('className', 'news-div');
-        newsDiv.innerHTML = `
+      const newsDiv = document.createElement('div');
+      newsDiv.setAttribute('className', 'news-div');
+      newsDiv.innerHTML = `
          <div className="card" style="width: 18rem;" >
          <img src="${article.urlToImage}" class="card-img-top" alt="picture">
          <div class="card-body">
            <p class="card-text">${article.title}</p>
-           <button class="btn-secondary like-review">
-           <i class="fa fa-heart" aria-hidden="true"></i> Like
-         </button>
          </div>
        </div>
          `;
-        // let li = document.createElement('li');
-        // let a = document.createElement('a');
-        // a.setAttribute('href', article.url);
-        // a.setAttribute('target', '_blank');
-        // a.textContent = article.title;
-        // li.appendChild(a);
-        // newsList.appendChild(li);
-        newsMainDiv.appendChild(newsDiv);
-    // container.innerHTML = result.info;
+      container.appendChild(newsDiv);
+    });
   } catch (error) {
     console.log('Error', error);
   }
 };
+
+req();
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,15 +5,14 @@ const Layout = require('./Layout');
 module.exports = function Home({ newUser }) {
   return (
     <Layout newUser={newUser}>
+      <script defer src="/js/home.js" />
       <h1>
         Hello user!
         {' '}
         { newUser }
       </h1>
       <div className="container">
-        s
       </div>
-      <script src="https://newsapi.org/v2/top-headlines?country=ru&apiKey=69f4633ca37b401eab74f2217a474d14" async defer />
     </Layout>
   );
 };
