cae86e5cc5f797ecbd95d74b4035650d09ac29b7
Ilya <shiroku12@mail.ru>
2022-10-05 09:50:55 +0300

    Rele60%

--- public/js/home.js
+++ public/js/home.js
@@ -7,6 +7,29 @@ const req = async () => { // ! Прикепел, оказывается гугл
     console.log(response);
     const result = await response.json();
     console.log(result);
+
+    result.articles.forEach((article) => {
+        const newsDiv = document.createElement('div');
+        newsDiv.setAttribute('className', 'news-div');
+        newsDiv.innerHTML = `
+         <div className="card" style="width: 18rem;" >
+         <img src="${article.urlToImage}" class="card-img-top" alt="picture">
+         <div class="card-body">
+           <p class="card-text">${article.title}</p>
+           <button class="btn-secondary like-review">
+           <i class="fa fa-heart" aria-hidden="true"></i> Like
+         </button>
+         </div>
+       </div>
+         `;
+        // let li = document.createElement('li');
+        // let a = document.createElement('a');
+        // a.setAttribute('href', article.url);
+        // a.setAttribute('target', '_blank');
+        // a.textContent = article.title;
+        // li.appendChild(a);
+        // newsList.appendChild(li);
+        newsMainDiv.appendChild(newsDiv);
     // container.innerHTML = result.info;
   } catch (error) {
     console.log('Error', error);
