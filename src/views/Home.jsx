448c327cd189db89af6ab20a55c2ba977d42687e
Lyudmila <zoruk.ll@gmail.com>
2022-10-07 12:41:27 +0300

    cat love

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,7 +6,6 @@ module.exports = function Home({ newUser }) {
   return (
     <Layout newUser={newUser}>
       <script defer src="/js/home.js" />
-
       <section className="section-home">
         <div className="home">
           <div className="home-text-box">
@@ -17,9 +16,8 @@ module.exports = function Home({ newUser }) {
             </h1>
             {/* <news portal </h2> */}
             <p className="home-decription"> Enjoy discovering the world with our free of charge news application. Seacrh from more then 80 news sources and save your favorite articles in one click! </p>
-            <a href="/registration">
-              <button className="glow-on-hover" type="button">WANNA STAY POSITIVE?</button>
-            </a>
+            <img className="clickCat" src="/css/images/clickCat.gif" />
+            <button id="clickCat" className="glow-on-hover" type="button">WANNA STAY POSITIVE?</button>
             {/* <a href="#" className='home-btn'>Start serching news</a> */}
           </div>
           <div className="borderNews">
